63697e97f2dfe04ee1a64820842b96c618c36d53
Aleksandr Ishutin <aleksandr.horosh93@gmail.com>
2023-10-30 16:19:12 +0400

    Lesson06

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+           <ul>
+            <li>Css</li>
+            <li>HTML</li>
+            <li>Js</li>
+            <li>React</li>
+           </ul>
+        </div>
+    );
 }
 
 export default App;
